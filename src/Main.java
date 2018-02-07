b6936a185f07a474a6cfd1393c7e808522cab51d
ethan <songyue2@illinois.edu>
2018-02-07 12:36:34 -0600

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class Main {
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        String article = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        char[] special = {'!', '@', '#', '$', '%', '^', '&', '*'};
+        //replaceall[]  (everything inside the square brackets will be removed, no comma needed
+        String[] words = article.trim().split("\\s+");
+        int wordcounter = words.length;
+        System.out.println(wordcounter);
     }
 }
